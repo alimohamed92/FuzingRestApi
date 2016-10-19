02a7fe6bc1cc06468aa8caef2442b7ba2fa657ad
AliMohamed <AliMohamed@DESKTOP-9VQHM03>
2016-10-19 11:01:38 +0200

    first commit

---
+++ FuzingRestApi/src/main/java/org/esir/AliMax/FuzingRestApi/App.java
@@ -0,0 +1,13 @@
+package org.esir.AliMax.FuzingRestApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
